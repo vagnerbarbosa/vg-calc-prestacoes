f817c201bd24316df4d9e4446b4357c57f1003db
Vagner Barbosa <contato@vagnerbarbosa.com>
2015-07-09 17:36:45 -0300

    Signed-off-by: Vagner Barbosa <contato@vagnerbarbosa.com>

---
+++ src/main/java/br/com/lojaoriodopeixe/vg/calc/utils/Utils.java
@@ -0,0 +1,62 @@
+package br.com.lojaoriodopeixe.vg.calc.utils;
+
+
+
+import java.awt.AWTKeyStroke;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.KeyboardFocusManager;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.util.HashSet;
+import javax.swing.JFrame;
+
+/**
+ * Classe contendo métodos que são úteis em determinadas partes do projeto
+ *
+ * @author Vagner Barbosa (contato@vagnerbarbosa.com)
+ *
+ * @since 07/06/2015
+ *
+ * @version 0.9
+ */
+public class Utils {
+
+    public Utils() {
+    }
+
+    public boolean caracterCheck(String value) {
+        if (value == null) {
+            return false;
+        }
+        for (char character : value.toCharArray()) {
+            if (character == ',') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void centerWindow(Component frame) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension frameSize = frame.getSize();
+
+        if (frameSize.width > screenSize.width) {
+            frameSize.width = screenSize.width;
+        }
+
+        if (frameSize.height > screenSize.height) {
+            frameSize.height = screenSize.height;
+        }
+
+        frame.setLocation((screenSize.width - frameSize.width) >> 1, (screenSize.height - frameSize.height) >> 1);
+    }
+
+    public void nextFieldWithEnter(JFrame frame) {
+        // Colocando enter para pular de campo
+        HashSet conj = new HashSet(frame.getFocusTraversalKeys(KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS));
+        conj.add(AWTKeyStroke.getAWTKeyStroke(KeyEvent.VK_ENTER, 0));
+        frame.setFocusTraversalKeys(KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS, conj);
+    }
+
+}
